240630eb4c309239230420eb6e3047a546ab5211
Tafita Rakotoarisoa Randriamirado <randriamirado.tafita@gmail.com>
2026-02-09 22:14:26 +0300

    implement logout
    frontend:
    - bouton logout appelle foction logout (clean localstorage)

--- frontend/src/app/layout/sidebar/sidebar.ts
+++ frontend/src/app/layout/sidebar/sidebar.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarService } from '../../shared/service/sidebar.service';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule], 
+  imports: [CommonModule, RouterLink],
   templateUrl: './sidebar.html',
-  styleUrls: ['./sidebar.css'] 
+  styleUrls: ['./sidebar.css']
 })
 export class Sidebar implements OnInit {
- currentMenu: string = 'default';
+  currentMenu: string = 'default';
 
-  constructor(private sidebarService: SidebarService) {}
+  constructor(private sidebarService: SidebarService) { }
 
   ngOnInit() {
     this.sidebarService.menu$.subscribe(menu => {
